8c5b4d7f76d8943d6520f6c8c08b932fc35b9a62
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-18 09:07:58 +0900

    duration, deley

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -69,3 +69,23 @@
 //     y: "10rem"
 //   })
 // }, 3000)
+
+// duration, deley
+
+// duration => アニメーションにかかる時間
+// durationの設定は、第二引数に設定するか、オブジェクトに書くか。
+gsap.to(".box", {
+  x: "20rem",
+  duration: 2
+})
+
+// gsap.to(".box", 10, {
+//   x: "20rem",
+// })
+
+
+// // deley => アニメーションが始まるまでのどれくら延滞するか。
+// gsap.to(".box", 1, {
+//   x: "20rem",
+//   delay: 1
+// })
