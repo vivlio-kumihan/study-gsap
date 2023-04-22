7b2d907043127db08c83d457981df710571a40e2
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-22 16:11:52 +0900

    複数の要素をずらして表示させる=> stagger

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -103,3 +103,136 @@
 
 // transformOrigin（変化の起点）について
 // opacityとautoAlphaについて
+
+// GSAPに用意されているCSSのtransformと同じプロパティ
+// 移動  translate
+// 伸縮  scale
+// 回転  rotate
+// 歪み  skew
+
+// gsap.to(".box", 2, {
+//   // // translate
+//   // x: "10rem",
+//   // y: "10rem"
+
+//   // // scale
+//   // // センターセンターで拡大
+//   // scale: 1.5 
+//   // // x軸センター方向へ拡大
+//   // scaleX: 2
+//   // // y軸センター方向へ拡大
+//   // scaleY: .5
+
+//   // // rotate
+//   // // センターを支点に回転
+//   // rotation: "90deg"
+//   // // x軸で回転
+//   // rotationX: "180deg"
+//   // // y軸で回転
+//   // rotationY: "180deg"
+
+//   // // skew
+//   // // こんな設定すると面白い効果になるけど動きをコントロールしにくい。
+//   // skewX: "360deg",
+//   // skewY: "-360deg"
+// }) 
+
+// // CSSで設定されているプロパティを変えることができる。
+// gsap.to (".box", 2, {
+//   backgroundColor: "red",
+//   borderRadius: "50%",
+//   border: "2rem solid #eee",
+//   boxShadow: "0 0 2rem red",
+// })
+
+// // CSSのtransform-originをGSAPで設定。
+// // アニメーションの支点を任意に設定できる。
+// gsap.to(".box", 2, {
+//   rotation: "360deg",
+//   transformOrigin: "50% 100%",
+//   scaleX: 0
+// }) 
+
+// // 最終形態を透明にしても要素はまだそこにある。
+// // その下の要素へアクセスする必要がある場合、例えばクリックして何かする
+// // というような動作ができなくなる。
+// // これを回避するために、visibility: hidden; を設定する。
+// // ついでに、GSAPで関数を定義して実行させるやり方も覚える。
+// gsap.to(".box", 2, {
+//   scale: 3,
+//   opacity: 0, 
+//   onComplete: function() {
+//     document.querySelector(".box").style.visibility = "hidden"
+//   }
+// })
+
+// // GSAPには、予めプロパティが用意されている。
+// gsap.to(".box", 2, {
+//   scale: 100,
+//   rotation: "360deg",
+//   autoAlpha: 0  
+// })
+
+const headers = document.querySelectorAll(".header")
+for (headTwo of headers) {
+  let orgText = headTwo.lastElementChild
+  let lastChar = orgText.textContent.slice(-1)
+  let preText = orgText.textContent.slice(0, -1)
+  orgText.innerHTML = `${ preText }<span class="spacing-nomalize">${ lastChar }</span>`
+}
+
+gsap.set(".container > ul > li", {
+  y: "2rem",
+  opacity: 0
+})
+gsap.to(".container > ul > li", 2, {
+  y: 0,
+  opacity: 1,
+  rotation: "360deg",
+  scale: 10,
+  stagger: {
+    each: .1,
+    from: "center"
+  }
+})
+
+// -----
+gsap.set("#articles > .wrapper", {
+  y: "2rem",
+  opacity: 0
+})
+
+gsap.to("#articles > .wrapper", 2, {
+  y: 0,
+  opacity:1,
+  // ここれだけでもすでにカッコいいし…
+  // stagger: .1
+  // より複雑にする。
+  stagger: {
+    // each => 何秒ずらしてそれぞれの要素を表示していくか。
+    // each: .1
+
+    // amount => 指定した秒数間で含まれる全要素をずらして表示していく。
+    // amount: 1
+
+    // 『each: .1』と同じ
+    // each: .1,
+    // from: "start"
+
+    // 『each: .1』の逆向き
+    // each: .1,
+    // from: "end"
+
+    // center => 中央から端へ
+    // each: .1,
+    // from: "center"
+
+    // edges => 端から中央へ
+    // each: .1,
+    // from: "edges"
+    
+    // random => ランダムに
+    each: .1,
+    from: "random"
+  }
+})
