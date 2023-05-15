bfd507b48f1c1307edfc6b79da4f4e3a12522ab4
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-15 10:28:03 +0900

    gsap_ps_day#002 => 左から右へ実線を引く

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,6 +1,6 @@
-const line = document.getElementById("line-one")
+const line = document.getElementById("line-two")
 
-const orgPostion = { x: "-45px", y: "-45px" }
+const orgPostion = { x: "0", y: "0" }
 
 gsap.set(line, {
   x: orgPostion.x,
@@ -11,8 +11,11 @@ gsap.fromTo(line, 2, {
   x: orgPostion.x,
   y: orgPostion.y,
 }, {
-  x: "0",
-  y: "0",
-  rotation: "360deg",
-  transformOrigin: "top left"
+  width: "100%",
+  transformOrigin: "top left",
+  // 開始時と終了時の速度が遅くなるため、
+  // 滑らかなアニメーションを実現することができます。
+  // inOutの部分は、アニメーションの始まりと終わりの速度が
+  // 遅くなるように指定しています。
+  ease: "power2.inout"
 })
