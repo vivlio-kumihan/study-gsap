21af78795c2a3e1a289b9c743584148c81c1fa5d
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-12 11:00:13 +0900

    trigger要素が『任意の地点』でpinし、以降、任意『要素に追従』・スクロールアウトする効果

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,41 +1,35 @@
-const tl = gsap.timeline()
-gsap.defaults({
-  ease: 'power1.inOut',
-  duration: 1,
+// trigger要素が、
+// 『任意の地点』でpinし、以降、
+// 任意『要素に追従』して、
+// スクロールアウトする効果
+ScrollTrigger.create({
+  trigger: '.container.one',
+  // sassでも説明したが、
+  // ペラペラのボックスに、
+  // 底辺から20pxということは、
+  // 上の要素の底辺から20px食い込む位置に
+  // レイアウト『出来る』ということ。
+  // これを前提にすると、
+  // start、endそれぞれ100％の意味がわかる。
+  start: 'top 100%',
+  endTrigger: '.space.end',
+  end: 'top 100%', 
+  pin: true,
+  // pinSpacingありだとtrigger要素の高さと
+  // 同じ高さの空きがその要素の下に発生する。
+  pinSpacing: false,
+  // markers: true
 })
-// 経過時間: 0s
-// 単純に1秒後に動き始める。
-tl.to('.one', { x: 700 }, '+=1')
-// 1秒前に1秒かけて動く　つまり、2秒 - 1秒 => 1秒後に動き始める。
-tl.to('.two', { x: 700 }, '-=1')
-// 経過時間: 2s
-// 単純に1秒後に動き始める。
-tl.to('.three', { x: 700 }, '+=1')
-// 経過時間: 4s
-// 1秒前に1秒かけて動く　つまり、2秒 - 1秒 => 1秒後に動き始める。
-// つまり、単純に1秒後に動き始める。
-tl.to('.four', { x: 700 }, 'anchor')
-// 経過時間: 5s => つまり、5 - 2 = 3秒後に動く
-tl.to('.five', { x: 700 }, 'anchor-=2')
-// 前と同時に動かす属性値は'<'
-tl.to('.six', { x: 700 })
-// 3秒後に1秒間かけて動く。合計4秒
-tl.to('.seven', { x: 700 })
-// 意図しないといけない動きをするので注意。
-// 前を起点（1秒間）に、1秒前に1秒かけて動く。
-tl.to('.eight', { x: 700 }, '<-=1')
-// 自分を起点に、2秒前に1秒間かけて動く。
-tl.to('.nine', { x: 700 }, '-=2')
 
-
-
-
-
-// const circle = document.querySelectorAll('.circle')
-// gsap.from(circle, 1, {
-//   autoAlpha: 0,
-//   x: '100%',
-//   y: '100%',
-//   ease: 'Power1.easeInOut',
-//   stagger: .2
-// })
+ScrollTrigger.create({
+  trigger: '.box',
+  start: 'top 80%',
+  // ここをページの最後までと指定するのが肝
+  endTrigger: 'html',
+  end: 'bottom top',
+  toggleClass: {
+    targets: ['.box'],
+    className: 'active'
+  },
+  markers: true,
+})
