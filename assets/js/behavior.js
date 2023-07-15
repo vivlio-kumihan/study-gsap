29b5e1e11fb2922325c9fba1e5f6438ce8ecbd74
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-15 10:25:19 +0900

    スクロールに応じてヘッダー、メニューバーの出し入れ

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -11,3 +11,24 @@ ScrollTrigger.create({
   pin: true,
   // markers: true,
 })
+
+// スクロールしてHeroコンテンツを過ぎるとヘッダーがスクリーン上辺へ収納される動作。
+// これにハンバーガーメニューの出現と合わせて複合的な動作を作る。
+ScrollTrigger.create({
+  trigger: '.container',
+  start: '1px top',
+  end: document.body.innerHeight,
+  toggleClass: {
+    className: 'headerHideBtnAppear', 
+    targets: ['header', '.headerMenuBtn']
+  },
+  markers: true,
+})
+
+//リサイズがあったらScrollTriggerをリセットする（innerHeightが変わっている可能性があるため）
+window.addEventListener('resize', () => {
+  ScrollTrigger.refresh();
+})
+
+// for headerMenuBtn's 3 div(s)
+document.getElementById('headerMenuBtn').insertAdjacentHTML('afterbegin', '<a><div></div><div></div><div></div></a>')
