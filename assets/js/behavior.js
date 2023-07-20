9ebb040fbf4d4950c58bf69495fcee0dcf3519f2
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-20 22:52:51 +0900

    縦から横、そして縦のスクロール

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,68 +1,19 @@
-gsap.registerPlugin(ScrollTrigger);
+const items = document.querySelectorAll('.js-item')
+const num = items.length
 
-// div.container要素をインスタンスにする。
-const pageContainer = document.querySelector(".container");
+gsap.set('#js-wrap', { width: num * 100 + '%' })
+gsap.set(items, { width: 100 / num + '%' })
 
-// 次にLocomotiveScrollの初期化をする。
-// SmoothScrollの機能を使用したいので『smooth: true』を設定。
-// 『el』には『SmoothScroll』を作動させたい領域を指定する。
-const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true
-});
-
-// LocomotiveScroll内でscrollイベントが走る度に、ScrollTriggerも更新させる。
-scroller.on("scroll", ScrollTrigger.update);
-
-// 『SmoothScroll』と『LocomotiveScroll』で内部要素の高さを同調する設定
-// 『SmoothScroll』を作動させると『LocomotiveScroll』がスクロールを制御するようになる。
-// 『el』で指定した部分で色々な高さの調整が行われるので、
-// 『ScrollTrigger』でもその値を使用するようにする。
-// 『LocomotiveScroll』を初期化した際のDOM要素と同じものを指定する。
-ScrollTrigger.scrollerProxy(pageContainer, {
-  scrollTop(value) {
-    return arguments.length
-      ? scroller.scrollTo(value, 0, 0)
-      : scroller.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      left: 0,
-      top: 0,
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  },
-  pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-
-//////////////////////////////////
-window.addEventListener("load", function () {
-  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
-  let pinWrap = document.querySelector(".pin-wrap");
-  let pinWrapWidth = pinWrap.offsetWidth;
-  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-
-  // Pinning and horizontal scrolling
-
-  gsap.to(".pin-wrap", {
-    scrollTrigger: {
-      scroller: pageContainer, //locomotive-scroll
-      scrub: true,
-      trigger: "#sectionPin",
-      pin: true,
-      // anticipatePin: 1,
-      start: "top top",
-      end: pinWrapWidth
-    },
-    x: -horizontalScrollLength,
-    ease: "none"
-  });
-
-  // そしてwindowが更新される度に『ScrollTrigger』『LocomotiveScroll』の
-  // 両方を更新するようにする。
-  // 全ての更新が終わり『LocomotiveScroll』で高さの調整が終わった後、
-  // 最後に『ScrollTrigger』を『refresh』する。
-  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-  ScrollTrigger.refresh();
-});
+gsap.to(items, {
+  xPercent: -100 * (num - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '#js-area',
+    start: 'top top',
+    end: '+=1000',
+    pin: true,
+    pinSpacing: false,
+    scrub: true,
+    markers: true
+  }
+})
